0aff84a80aa913c34e637935428e92e27f358a10
maxgrossman <m.grossman1813@gmail.com>
2017-10-02 10:19:18 -0400

    update s3 upload to upload csv admin dumps

---
+++ scripts/make-analysis-and-display.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+set -e
+
+echo "Ensure the necessary environment variables are set"
+# Credentials
+: "${DATABASE_URL:?}"
+: "${AWS_ACCESS_KEY_ID:?}"
+: "${AWS_SECRET_ACCESS_KEY:?}"
+: "${MAPBOX_ACCESS_TOKEN:?}"
+# Paths
+: "${S3_DUMP_BUCKET:?}"
+: "${S3_PROPERTIES_BUCKET:?}"
+: "${MAPBOX_ACCOUNT:?}"
+
+# Change to script's directory
+cd "${0%/*}"
+
+mkdir -p .tmp
+
+echo "Dumping ways from database"
+psql "$DATABASE_URL" < ways.sql
+
+echo "Converting network to GeoJSON"
+mkdir .tmp/network
+./to-geojson.js .tmp/waynodes.csv .tmp/waytags.csv > .tmp/network.geojson
+split --lines 1 .tmp/network.geojson ".tmp/network/"
+../node_modules/.bin/geojson-merge \
+    .tmp/network/* \
+    > .tmp/network-merged.geojson
+
+echo "Get all point-property data from S3"
+mkdir .tmp/points
+aws s3 cp \
+    --recursive --include "*.geojson" \
+    "s3://${S3_PROPERTIES_BUCKET}/points" \
+    .tmp/points
+../node_modules/.bin/geojson-merge \
+    .tmp/points/*.geojson \
+    > .tmp/points.geojson
+../node_modules/.bin/reproject \
+    --use-spatialreference --from EPSG:32648 --to EPSG:4326 \
+    .tmp/points.geojson \
+    > .tmp/points-wgs84.geojson
+
+echo "Conflate point data's core OR attributes onto network lines"
+./conflate-points-lines.js \
+    .tmp/network-merged.geojson \
+    .tmp/points-wgs84.geojson \
+    .tmp/conflated.geojson
+
+echo "Convert to vector tiles, and upload to Mapbox"
+tippecanoe \
+    --layer "conflated" \
+    --minimum-zoom 0 --maximum-zoom 16 \
+    --drop-smallest-as-needed \
+    --force --output ".tmp/conflated.mbtiles" \
+    ".tmp/conflated.geojson"
+mapbox upload \
+    "${MAPBOX_ACCOUNT}.vietnam-conflated" \
+    ".tmp/conflated.mbtiles"
+
+echo "There is currently no analytics processing, but it would be insterted here"
+
+echo "Dump un-conflated, by-province data to S3, for public consumption"
+mkdir .tmp/by-province-id
+# ./to-admin-geojson.js .tmp/network-merged.geojson .tmp/by-province-id
+./to-admin-csv.js .tmp/network-merged.geojson .tmp/by-province-id
+aws s3 cp \
+    --recursive \
+    .tmp/by-province-id \
+    "s3://${S3_DUMP_BUCKET}/by-province-id" \
+    --acl public-read
+
+echo "Successfully finished"
--- scripts/to-admin-geojson.js
+++ scripts/to-admin-geojson.js
@@ -1,19 +1,16 @@
+#!/usr/bin/env node
 'use strict';
 // writes road geojsons grouped by admin.
 
 var map = require('lodash.map');
 var groupBy = require('lodash.groupby');
-var flatten = require('flat');
 var featureCollection = require('@turf/helpers').featureCollection;
 var fs = require('fs');
 var geojsonStream = require('geojson-stream');
-var json2csv = require('json2csv');
-var wkx = require('wkx');
 
 Promise = require('bluebird');
 
 const parser = geojsonStream.parse();
-const mappedSubadmin = JSON.parse(fs.readFileSync('lib/vietnam-admin.subadmin.json').toString());
 
 let mappedFeatures = [];
 
@@ -24,13 +21,9 @@ fs.createReadStream(process.argv[2])
     const rollupObj = {};
     // only push features to mappedFeatures where or_vpromms exist.
     if (feature.properties.or_vpromms) {
-      const subCode = feature.properties.or_vpromms.slice(3, 5);
-      let adminCode = mappedSubadmin.filter((mapObj) => {
-        if (mapObj.subCode === subCode) {
-          return mapObj.adminCode;
-        }
-      })[0].adminCode;
-      rollupObj['admin'] = adminCode;
+      const adminID = feature.properties.or_vpromms.slice(0, 2);
+      rollupObj['admin'] = adminID;
+>>>>>>> d3e032927b34708b09bf4c985013d5ad4656d893
       rollupObj['feature'] = feature;
       mappedFeatures.push(rollupObj);
     }
@@ -41,30 +34,14 @@ fs.createReadStream(process.argv[2])
       return mappedFeature.admin;
     });
     // for each group, write features as feature collection to geojson.
-    Promise.each(Object.keys(mappedFeatures), (key, i) => {
-      let features = map(mappedFeatures[key], 'feature')
-      let fields;
-      features = features.map((feature, i) => {
-        var f = feature;
-        var newF = {};
-        var geometry = wkx.Geometry.parseGeoJSON(feature.geometry).toWkt()
-        delete f.geometry
-        f = flatten(f)
-        f.geometry = geometry
-        // remove properties from object keys
-        f = Object.keys(f).forEach((k) => {
-          let newK = k;
-          if (/properties/.test(k)) {
-            newK = k.replace('properties.', '')
-          }
-          newF[newK] = f[k];
-          if (i === 0) { fields = Object.keys(newF) }
-        })
-        return newF
-      })
-      var featuresCSV = json2csv({ data: features, fields: fields });
-      const fileName = `${process.argv[3]}/${mappedFeatures[key][0].admin}.csv`;
-      fs.writeFileSync(fileName, featuresCSV);
+
+    Promise.each(Object.keys(mappedFeatures), (key) => {
+      const fc = featureCollection(
+        map(mappedFeatures[key], 'feature')
+      );
+      const fileName = `${process.argv[3]}/${mappedFeatures[key][0].admin}.geojson`;
+      fs.writeFileSync(fileName, JSON.stringify(fc));
+>>>>>>> d3e032927b34708b09bf4c985013d5ad4656d893
     }).then(() => {
       console.log('end');
     });
