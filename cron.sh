be0617997449c2c35c3cd4b2728363faccb76d48
Matthew Hanson <matt.a.hanson@gmail.com>
2017-10-04 23:24:45 -0400

    update cron script

--- cron.sh
+++ cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'export DATABASE_URL=$DATABASE_URL' >> .env
-echo 'export S3_BUCKET=$S3_BUCKET' >> .env
-echo 'export PATH=$PATH:/usr/local/bin' >> .env
+echo export DATABASE_URL=$DATABASE_URL >> .env
+echo export S3_BUCKET=$S3_BUCKET >> .env
+echo export PATH=$PATH:/usr/local/bin >> .env
 echo '*/5 * * * * . /opt/app/.env; cd /opt/app; node index.js >> /opt/app/cron.log 2>&1' | crontab
 cron start -f
