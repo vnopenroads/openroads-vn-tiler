c0aada5629f0f620c32970f7afc9ad0dc9b714fa
Matthew Hanson <matt.a.hanson@gmail.com>
2017-10-06 17:56:29 -0400

    use own tmpdirectory and replace in ways.sql before piping to psql

--- scripts/make-analysis-and-display.sh
+++ scripts/make-analysis-and-display.sh
@@ -14,7 +14,8 @@ echo "Ensure the necessary environment variables are set"
 # Change to script's directory
 cd "${0%/*}"
 
-mkdir -p .tmp
+WORKDIR=make-analysis.tmp
+mkdir -p $WORKDIR
 
 echo "Run analytics calculations"
 # This is the only location in this script where the database is _written to_
@@ -22,42 +23,42 @@ psql "$DATABASE_URL" < calculate-iri-summary-stats.sql
 psql "$DATABASE_URL" < calculate-lengths.sql
 
 echo "Dumping ways and properties from database"
-psql "$DATABASE_URL" < ways.sql
+cat ways.sql | sed -e 's/.tmp/'"$WORKDIR"'/g' ways.sql | psql $DATABASE_URL
 
 echo "Converting network to GeoJSON"
-mkdir -p .tmp/network
-./to-geojson.js .tmp/waynodes.csv .tmp/waytags.csv .tmp/road_properties.csv > .tmp/network.geojson
-split --lines 1 .tmp/network.geojson ".tmp/network/"
+mkdir -p $WORKDIR/network
+./to-geojson.js $WORKDIR/waynodes.csv $WORKDIR/waytags.csv $WORKDIR/road_properties.csv > $WORKDIR/network.geojson
+split --lines 1 $WORKDIR/network.geojson "$WORKDIR/network/"
 ../node_modules/.bin/geojson-merge \
-    .tmp/network/* \
-    > .tmp/network-merged.geojson
+    $WORKDIR/network/* \
+    > $WORKDIR/network-merged.geojson
 
 echo "Conflate point data's core OR attributes onto network lines"
 ./conflate-points-lines.js \
-    .tmp/network-merged.geojson \
-    .tmp/points.geojson \
-    .tmp/conflated.geojson
+    $WORKDIR/network-merged.geojson \
+    $WORKDIR/points.geojson \
+    $WORKDIR/conflated.geojson
 
 echo "Convert to vector tiles, and upload to Mapbox"
 tippecanoe \
     --layer "conflated" \
     --minimum-zoom 0 --maximum-zoom 16 \
     --drop-smallest-as-needed \
-    --force --output ".tmp/conflated.mbtiles" \
-    ".tmp/conflated.geojson"
+    --force --output "$WORKDIR/conflated.mbtiles" \
+    "$WORKDIR/conflated.geojson"
 mapbox upload \
     "${MAPBOX_ACCOUNT}.vietnam-conflated" \
-    ".tmp/conflated.mbtiles"
+    "$WORKDIR/conflated.mbtiles"
 
 echo "Dump un-conflated, by-province data to S3, for public consumption"
-mkdir -p .tmp/by-province-id
-./to-admin-csv.js .tmp/network-merged.geojson .tmp/by-province-id
+mkdir -p $WORKDIR/by-province-id
+./to-admin-csv.js $WORKDIR/network-merged.geojson $WORKDIR/by-province-id
 aws s3 cp \
     --recursive \
-    .tmp/by-province-id \
+    $WORKDIR/by-province-id \
     "s3://${S3_DUMP_BUCKET}/by-province-id" \
     --acl public-read
 
-rm -rf .tmp
+rm -rf $WORKDIR
 
 echo "Successfully finished"
