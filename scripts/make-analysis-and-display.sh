dddaf9e40e8ab442b281a583cc511b4191bc834b
Sajjad Anwar <sajjadkm@gmail.com>
2018-10-18 23:16:51 +0530

    remove national road conflation. simplify tileset generation

--- scripts/make-analysis-and-display.sh
+++ scripts/make-analysis-and-display.sh
@@ -27,10 +27,11 @@ cat ways.sql | sed -e 's/.tmp/'"$WORKDIR"'/g' ways.sql | psql "$DATABASE_URL"
 echo "Converting network to GeoJSON"
 mkdir -p $WORKDIR/network
 ./to-geojson.js $WORKDIR/waynodes.csv $WORKDIR/waytags.csv $WORKDIR/road_properties.csv > $WORKDIR/network.geojson
-split --lines 1 $WORKDIR/network.geojson "$WORKDIR/network/"
-../node_modules/.bin/geojson-merge \
-    $WORKDIR/network/* \
-    > $WORKDIR/network-merged.geojson
+
+## No need to conflate so not merging anymore. See below.
+# gsplit --lines 1 $WORKDIR/network.geojson "$WORKDIR/network/"
+# ../node_modules/.bin/geojson-merge $WORKDIR/network/* > $WORKDIR/network-merged.geojson
+# geojson-stream-merge --input $WORKDIR/network.geojson --output $WORKDIR/network-merged.geojson
 
 echo "Creating export for CBA"
 ./create-cba-export.js $WORKDIR/network.geojson > $WORKDIR/orma-sections.csv
@@ -40,22 +41,25 @@ aws s3 cp \
     "${WORKDIR}/orma-sections.csv" \
     "s3://${S3_DUMP_BUCKET}/cba/orma-sections-$(date +%Y-%m-%d).csv"
 
-echo "Downloading national highways, which aren't tracked in ORMA"
-aws s3 cp \
-    "s3://$S3_DUMP_BUCKET/private-fixture-data/National_network.geojson" \
-    "$WORKDIR/National_network.geojson"
+## Note - since we're using WON, we don't need to conflate with the national network.
+## National network will now be moved to another tileset on Mapbox.
 
-echo "Conflate point data's core OR attributes onto network lines"
-./conflate-points-lines.js \
-    $WORKDIR/network-merged.geojson \
-    $WORKDIR/points.geojson \
-    $WORKDIR/conflated.geojson
+# echo "Downloading national highways, which aren't tracked in ORMA"
+# aws s3 cp \
+#     "s3://$S3_DUMP_BUCKET/private-fixture-data/National_network.geojson" \
+#     "$WORKDIR/National_network.geojson"
 
-echo "Merging conflated ORMA roads and national highways into one network file"
-../node_modules/.bin/geojson-merge \
-    $WORKDIR/National_network.geojson \
-    $WORKDIR/conflated.geojson > \
-    $WORKDIR/all-roads.geojson
+# echo "Conflate point data's core OR attributes onto network lines"
+# ./conflate-points-lines.js \
+#     $WORKDIR/network-merged.geojson \
+#     $WORKDIR/points.geojson \
+#     $WORKDIR/conflated.geojson
+
+# echo "Merging conflated ORMA roads and national highways into one network file"
+# ../node_modules/.bin/geojson-merge \
+#     $WORKDIR/National_network.geojson \
+#     $WORKDIR/conflated.geojson > \
+#     $WORKDIR/all-roads.geojson
 
 echo "Convert to vector tiles, and upload to Mapbox"
 tippecanoe \
@@ -63,7 +67,7 @@ tippecanoe \
     --minimum-zoom 0 --maximum-zoom 16 \
     --drop-smallest-as-needed \
     --force --output "$WORKDIR/conflated.mbtiles" \
-    "$WORKDIR/all-roads.geojson"
+    "$WORKDIR/network.geojson"
 
 export MapboxAccessToken=$MAPBOX_ACCESS_TOKEN
 mapbox-upload \
