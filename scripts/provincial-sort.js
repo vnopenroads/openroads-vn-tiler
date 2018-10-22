9f2f4467746e6dbb629ff72e3a52db81677e0cd4
Sanjay Bhangar <sanjaybhangar@gmail.com>
2018-10-22 16:35:44 +0530

    write to separate csv files per province id

--- scripts/provincial-sort.js
+++ scripts/provincial-sort.js
@@ -12,6 +12,14 @@ const _ = require('lodash');
 const headers = ["province", "vpromm_id", "way_id", "geom", "iri_min", "iri_max", "iri_mean", "iri_med", "iri_stdev", "speed_mean", "length"];
 
 const properties = ["iri_min", "iri_max", "iri_mean", "iri_med", "iri_stdev", "speed_mean", "length"];
+let provinceFilesRemoved = [];
+
+const OUTPUT_DIR = '/tmp/provinces/';
+
+function getProvincePath(id) {
+    return OUTPUT_DIR + id + '.csv';
+}
+
 var roads = fs.createReadStream(process.argv[2])
     .pipe(csv())
     .pipe(through.obj((obj, enc, next) => {
@@ -23,9 +31,31 @@ var roads = fs.createReadStream(process.argv[2])
         row['vpromm_id'] = obj['vpromm_id'];
         row['way_id'] = obj['way_id'];
         row['geom'] = obj['geom'];
-        next(null, row)
+        next(null, row);
     }))
 
     // to-do: stringify but write to a file based on province
-    .pipe(stringifier({columns: headers}))
-    .pipe(process.stdout);
+    .pipe(through.obj((obj, enc, next) => {
+        const arr = headers.map(col => {
+            return obj[col] || '';
+        });
+        stringifier([arr], (err, output) => {
+            const filePath = getProvincePath(obj['province']);
+            // remove previous province file if exists and not already created in this run
+            if (provinceFilesRemoved.indexOf(obj['province']) === -1 && fs.existsSync(filePath)) {
+                fs.unlinkSync(filePath);
+                provinceFilesRemoved.push(obj['province']);
+            }
+            return next(null, {
+                province: obj['province'],
+                data: output
+            });
+        });
+    }))
+    .pipe(through.obj((obj, enc, next) => {
+        const provinceId = obj['province'];
+        const csvString = obj['data'];
+        const filePath = getProvincePath(provinceId);
+        fs.appendFileSync(filePath, csvString);
+        next();
+    }));
