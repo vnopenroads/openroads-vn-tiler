7528a143065792ecabe0bbdd72228555a0e87505
Sajjad Anwar <sajjadkm@gmail.com>
2018-10-22 20:29:43 +0530

    add to cron. run everyday.

--- cron.sh
+++ cron.sh
@@ -11,6 +11,6 @@ echo export MAPBOX_ACCESS_TOKEN=$MAPBOX_ACCESS_TOKEN >> .env
 echo '*/5 * * * * . /opt/app/.env; cd /opt/app; ./scripts/make-network.sh >> make-network.log 2>&1' | crontab -
 (crontab -l; echo '*/30 * * * * . /opt/app/.env; cd /opt/app; ./scripts/make-analysis-and-display.sh >> make-analysis-and-display.log 2>&1') | crontab -
 (crontab -l; echo '* */24 * * * . /opt/app/.env; cd /opt/app; ./scripts/generate-tasks.sh >> generate-tasks.log 2>&1') | crontab -
-(crontab -l; echo '* */2 * * * . /opt/app/.env; cd /opt/app; ./scripts/generate-provincial-dumps.sh >> generate-provincial-dumps.log 2>&1') | crontab -
+(crontab -l; echo '* */24 * * * . /opt/app/.env; cd /opt/app; ./scripts/generate-provincial-dumps.sh >> generate-provincial-dumps.log 2>&1') | crontab -
 (crontab -l; echo '* */24 * * * . /opt/app/.env; cd /opt/app; ./scripts/make-full-backup.sh >> make-full-backup.log 2>&1') | crontab -
 cron start -f
